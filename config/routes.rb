240211176023410545f867652edb4db93d209923
vibesharing <buret.hadrien@orange.fr>
2016-11-02 18:07:06 +0100

    destinations model | controllers | routes params / edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get 'welcome' => 'pages#home'
   get 'tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag
+  get '/destinations/:id' => 'destinations#show', as: :destination
+
+  get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination
+  patch '/destinations/:id' => 'destinations#update'
 end
