3d232913570f3ada9deeab1e35c48a79de37a204
vibesharing <buret.hadrien@orange.fr>
2016-11-02 14:33:44 +0100

    tag working

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome' => 'pages#home'
   get 'tags' => 'tags#index'
+  get '/tags/:id' => 'tags#show', as: :tag
 end
